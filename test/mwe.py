af7a07af3e67c7667582c32f1e6680db09044306
Jeongjin Park <jpark3141@atl1-1-02-009-34-0.pace.gatech.edu>
2024-04-25 16:08:14 -0400

    updated mwe.py

--- test/mwe.py
+++ test/mwe.py
@@ -9,7 +9,7 @@ _ = torch.manual_seed(0)
 
 # 1. A simple linear function
 def predict(weight, bias, x):
-    return F.linear(x, weight, bias)#.tanh()
+    return 0.5 * torch.linalg.norm(F.linear(x, weight, bias), 2)**2 #.tanh()
 
 # 2. Dummy data
 D = 3
@@ -28,20 +28,19 @@ torch.func.jacrev: performs vmap-vjp composition to compute Jacobians.
 Step 1. Compute df/dx
 '''
 
-jacobian_input = jacrev(predict, argnums=2)(weight, bias, x)
-
+jacobian_input = lambda x: jacrev(predict, argnums=2)(weight, bias, x)
 print("Step 1: ", jacobian_input, "\n")
 
 '''
 Step 2. Compute h(f) = 1/2||f(x)||^2_2
 '''
 
-norm = lambda v: 0.5 * torch.linalg.norm(v, 2)**2
-print("Step 2: ", norm(jacobian_input), "\n")
+# norm = lambda v: 0.5 * torch.linalg.norm(v, 2)**2
+# print("Step 2: ", norm(jacobian_input), "\n")
 
 '''
 Step 3. Compute dh/df
 '''
 
-jacobian_param = jacrev(norm, argnums=0)(jacobian_input)
+jacobian_param = jacrev(jacobian_input, argnums=0)(weight)
 print("Step 3: ", jacobian_param)
