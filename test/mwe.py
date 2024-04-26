4aad4e90e9eb44b207c5e995396d627b5cd08e77
Jeongjin Park <jpark3141@atl1-1-02-009-34-0.pace.gatech.edu>
2024-04-26 01:36:41 -0400

    updated mwe.py

--- test/mwe.py
+++ test/mwe.py
@@ -12,12 +12,13 @@ def predict(weight, bias, x):
     return 0.5 * torch.linalg.norm(F.linear(x, weight, bias), 2)**2 #.tanh()
 
 # 2. Dummy data
-D = 3
+D = 2
 weight = torch.randn(D, D)
 bias = torch.randn(D)
 x = torch.randn(D)
 print("weight: ", weight)
 print("input: ", x, "\n")
+print("bias: ", bias, "\n")
 
 
 '''
@@ -28,15 +29,12 @@ torch.func.jacrev: performs vmap-vjp composition to compute Jacobians.
 Step 1. Compute df/dx
 '''
 
-jacobian_input = lambda x: jacrev(predict, argnums=2)(weight, bias, x)
-print("Step 1: ", jacobian_input, "\n")
+jacobian_input = lambda weight: 0.5 * torch.linalg.norm(jacrev(predict, argnums=2)(weight, bias, x), 2)**2
 
 '''
 Step 2. Compute h(f) = 1/2||f(x)||^2_2
 '''
 
-# norm = lambda v: 0.5 * torch.linalg.norm(v, 2)**2
-# print("Step 2: ", norm(jacobian_input), "\n")
 
 '''
 Step 3. Compute dh/df
@@ -44,3 +42,12 @@ Step 3. Compute dh/df
 
 jacobian_param = jacrev(jacobian_input, argnums=0)(weight)
 print("Step 3: ", jacobian_param)
+
+
+'''
+Step 4. Sanity Check: compute the analytical solution
+'''
+
+g = torch.matmul(weight.T, weight*x+bias)
+final = torch.matmul(g,(weight*x + bias).T * x)
+print("final", final)
